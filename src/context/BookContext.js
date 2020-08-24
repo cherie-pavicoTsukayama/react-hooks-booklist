d097cf3a913683de5d23ebbc0cb32136a4280d48
Cherie <cherietsukayama@gmail.com>
2020-08-24 21:16:21 +0000

    refactored code to include a reducer file. Refactored BookDetails and BookForm to use the reducer and dispatch methods.

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,22 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useReducer } from 'react';
 import {v4 as uuidv4} from 'uuid';
+import bookReducer from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBook] = useState([
-    {title: 'Name of the Wind', author: 'Patric rothfuss', id: 1},
-    {title: 'The Final Empire', author: 'Brandon Sanderson', id: 2},
-  ])
+  const [books, dispatch] = useReducer(bookReducer, [])
 
-  const addBook = (title, author) => {
-    setBook([...books, {title, author, id: uuidv4()}])
-  }
-  const removeBook = (id) => {
-    setBook(books.filter(book => book.id !== id));
-  }
+  // const addBook = (title, author) => {
+  //   setBook([...books, {title, author, id: uuidv4()}])
+  // }
+  // const removeBook = (id) => {
+  //   setBook(books.filter(book => book.id !== id));
+  // }
   return (
-    <BookContext.Provider value={ {books, addBook, removeBook} }>
+    <BookContext.Provider value={ {books, dispatch} }>
       { props.children }
     </BookContext.Provider>
   )
